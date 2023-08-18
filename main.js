36cfbfcb1c7574400721df65553b16372225dae2
Rocky <584601022@qq.com>
2023-08-18 23:22:21 +0800

    引入动画

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import App from './App'
+import "animate.css"
 
 // #ifndef VUE3
 import Vue from 'vue'
@@ -6,17 +7,19 @@ import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
