05122c2098e2af73432cde1deb3641138e53dfef
Rocky <lanzhe9527@163.com>
2023-08-20 16:54:37 +0800

    封装图标分类组件

---
+++ components/index/index-nav.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<!-- 首页分类 -->
+		<view class="index-class-box">
+			<block v-for="(item,index) in iconNav">
+			<view class="span-box">
+				<image :src="item.src" style="width: 60rpx; height: 60rpx;"></image>
+			<text>{{item.text}}</text>
+			</view>
+			</block>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			iconNav:Array
+		}
+	}
+</script>
+
+<style>
+	.index-class-box{
+		display: flex;
+		flex-wrap: wrap;
+		padding: 20rpx;
+	}
+	.span-box{
+		width: 20%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding:20rpx 0;
+	
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,8 @@
 	<view>
 		<!-- 轮播图 -->
 		<swiper-images :resdata="swipers"></swiper-images>
-		
+		<!-- 首页分类 -->
+		<nav-icon :iconNav="iconArray"></nav-icon>
 		
 		
 	</view>
@@ -10,15 +11,17 @@
 
 <script>
 	import swiperImages from "@/components/index/swiper-images.vue"
+	import navIcon from "@/components/index/index-nav.vue"
 	export default {
 		components:{
-			swiperImages
+			swiperImages,
+			navIcon
 		},
 		data() {
 			return {
 				swipers:[
 					{
-						src:"../../static/images/lc250.png"
+						src:"../../static/images/lc250.png",
 					},
 					{
 						src:"../../static/images/wrangler.png"
@@ -26,7 +29,51 @@
 					{
 						src:"../../static/images/goole.png"
 					}
+				],
+				
+				iconArray:[
+					{
+						src:"../../static/indexNav/1.png",
+						text:'新品发布'
+					},
+					{
+						src:"../../static/indexNav/2.gif",
+						text:'小米众筹'
+					},
+					{
+						src:"../../static/indexNav/3.gif",
+						text:'以旧换新'
+					},
+					{
+						src:"../../static/indexNav/4.gif",
+						text:'一分拼团'
+					},
+					{
+						src:"../../static/indexNav/5.gif",
+						text:'超值特卖'
+					},
+					{
+						src:"../../static/indexNav/6.gif",
+						text:'小米秒杀'
+					},
+					{
+						src:"../../static/indexNav/7.gif",
+						text:'真心想要'
+					},
+					{
+						src:"../../static/indexNav/8.gif",
+						text:'电视特卖'
+					},
+					{
+						src:"../../static/indexNav/9.gif",
+						text:'家电热卖'
+					},
+					{
+						src:"../../static/indexNav/10.gif",
+						text:'米粉卡'
+					},
 				]
+				
 			}
 		},
 		onLoad() {
