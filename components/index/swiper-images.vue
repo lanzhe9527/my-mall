26ab5f0f17ce6ad12af34d18625e7aa3099a38fb
Rocky <lanzhe9527@163.com>
2023-08-20 14:51:20 +0800

    封装轮播图

---
+++ components/index/swiper-images.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<!-- 轮播图 -->
+		<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="gray"
+				indicator-active-color="#FFF" circular>
+				<block v-for="(item,index) in resdata" :key="index">
+					<swiper-item>
+						<view class="swiper-item">
+							<image :src="item.src" style="height: 350rpx;"></image>
+						</view>
+					</swiper-item>
+				</block>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			resdata:Array
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,7 @@
 <template>
 	<view>
 		<!-- 轮播图 -->
-		<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="gray"
-				indicator-active-color="#FFF" circular>
-				<block v-for="(item,index) in swipers" :key="index">
-					<swiper-item>
-						<view class="swiper-item">
-							<image :src="item.src" style="height: 350rpx;"></image>
-						</view>
-					</swiper-item>
-				</block>
-		</swiper>
-
+		<swiper-images :resdata="swipers"></swiper-images>
 		
 		
 		
@@ -19,7 +9,11 @@
 </template>
 
 <script>
+	import swiperImages from "@/components/index/swiper-images.vue"
 	export default {
+		components:{
+			swiperImages
+		},
 		data() {
 			return {
 				swipers:[
