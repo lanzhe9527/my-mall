2f9a818647e07de9e76fbc23831b9a77f0f42ae1
Rocky <lanzhe9527@163.com>
2023-08-20 10:01:52 +0800

    完善轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,22 @@
 <template>
 	<view>
 		<!-- 轮播图 -->
-		<swiper indicator-dots autoplay :interval="3000" :duration="1000">
+		<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="gray"
+				indicator-active-color="#FFF" circular>
 			<swiper-item>
 				<view class="swiper-item" style="background-color: aqua;">
 					<image src="../../static/images/lc250.png" style="height: 350rpx;"></image>
 				</view>
 			</swiper-item>
 			<swiper-item>
-				<view class="swiper-item" style="background-color: bisque;">2</view>
+				<view class="swiper-item" style="background-color: bisque;">
+					<image src="../../static/images/wrangler.png" style="height: 350rpx;"></image>
+				</view>
 			</swiper-item>
 			<swiper-item>
-				<view class="swiper-item" style="background-color: bisque;">3</view>
+				<view class="swiper-item" style="background-color: bisque;">
+					<image src="../../static/images/goole.png" style="height: 350rpx;"></image>
+				</view>
 			</swiper-item>
 		</swiper>
 	</view>
