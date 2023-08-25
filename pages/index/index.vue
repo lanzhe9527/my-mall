5f0e807944ed3f49811f2407eb553a9689677ec4
Rocky <lanzhe9527@163.com>
2023-08-25 23:41:27 +0800

    模拟后端数据

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -57,26 +57,8 @@
 </template>
 
 <script>
-	import swiperImages from "@/components/index/swiper-images.vue"
-	import navIcon from "@/components/index/index-nav.vue"
-	import threeAdv from "@/components/index/three-adv.vue"
-	import card from "@/components/common/card.vue"
-	import commonList from "@/components/common/commList.vue"
-	export default {
-		components:{
-			swiperImages,
-			navIcon,
-			threeAdv,
-			card,
-			commonList
-		},
-		data() {
-			return {
-				scrollInto:'',
-				tabIndex:0,
-				scrollH:500,
-				tabBars:[
-					{
+	// 模拟后端数据
+	let demoTabars=[{
 						name:"关注"
 					},
 					{
@@ -102,14 +84,9 @@
 					},
 					{
 						name:"财经"
-					},
-				],
-				
-				newsItems:[
-					{
-						name:"关注",
-						list:[
-							{
+					},];
+					
+	let demo1=[{
 								type:"swiper",
 								data:[
 									{
@@ -228,156 +205,141 @@
 										nPrice:"1999"
 									}
 								]
+							},]
+	let demo2=[{
+								type:"swiper",
+								data:[
+									{
+										src:"../../static/images/lc250.png",
+									},
+									{
+										src:"../../static/images/wrangler.png"
+									},
+									{
+										src:"../../static/images/goole.png"
+									}
+								]
 							},
-						]
-					},
-					{
-						name:"推荐"
-					},
-					{
-						name:"体育"
-					},
-					{
-						name:"热点"
-					},
-					{
-						name:"财经"
-					},
-					{
-						name:"军事"
-					},
-					{
-						name:"越野"
-					},
-					{
-						name:"超跑"
-					},
-					{
-						name:"财经"
-					},
-				],
-				
-				
-				// swipers:[
-				// 	{
-				// 		src:"../../static/images/lc250.png",
-				// 	},
-				// 	{
-				// 		src:"../../static/images/wrangler.png"
-				// 	},
-				// 	{
-				// 		src:"../../static/images/goole.png"
-				// 	}
-				// ],
-				
-				// iconArray:[
-				// 	{
-				// 		src:"../../static/indexNav/1.png",
-				// 		text:'新品发布'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/2.gif",
-				// 		text:'小米众筹'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/3.gif",
-				// 		text:'以旧换新'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/4.gif",
-				// 		text:'一分拼团'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/5.gif",
-				// 		text:'超值特卖'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/6.gif",
-				// 		text:'小米秒杀'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/7.gif",
-				// 		text:'真心想要'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/8.gif",
-				// 		text:'电视特卖'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/9.gif",
-				// 		text:'家电热卖'
-				// 	},
-				// 	{
-				// 		src:"../../static/indexNav/10.gif",
-				// 		text:'米粉卡'
-				// 	},
-				// ],
-				// threeeAdv:{
-				// 	big:{
-				// 		src:"../../static/images/demo/demo1.jpg"
-				// 	},
-				// 	smallTop:{
-				// 		src:"../../static/images/demo/demo2.jpg"
-				// 	},
-				// 	smallBottom:{
-				// 		src:"../../static/images/demo/demo3.jpg"
-				// 	}
-				// },
-				// commonList:[
-					// {
-					// 	cover:"../../static/images/list/1.jpg",
-					// 	title:"米家空调",
-					// 	desc:"1.5匹变流空调",
-					// 	oPrice:"2678",
-					// 	nPrice:"1999"
-					// },
-					// {
-					// 	cover:"../../static/images/list/2.jpg",
-					// 	title:"黑鲨手机",
-					// 	desc:"游戏性能，散热好",
-					// 	oPrice:"2678",
-					// 	nPrice:"1299"
-					// },
-					// {
-					// 	cover:"../../static/images/list/3.jpg",
-					// 	title:"红米手机",
-					// 	desc:"1.5匹变流空调",
-					// 	oPrice:"2678",
-					// 	nPrice:"1999"
-					// },
-					// {
-					// 	cover:"../../static/images/list/4.jpg",
-					// 	title:"荣耀",
-					// 	desc:"1.5匹变流空调",
-					// 	oPrice:"2678",
-					// 	nPrice:"1999"
-					// },
-					// {
-					// 	cover:"../../static/images/list/5.jpg",
-					// 	title:"红米redmi",
-					// 	desc:"1.5匹变流空调",
-					// 	oPrice:"2678",
-					// 	nPrice:"1999"
-					// },
-					// {
-					// 	cover:"../../static/images/list/6.jpg",
-					// 	title:"米家平板",
-					// 	desc:"本地数据",
-					// 	oPrice:"2678",
-					// 	nPrice:"1999"
-					// }
-				// ]
+							{
+								type:"iconArray",
+								data:[
+							
+									{
+										src:"../../static/indexNav/6.gif",
+										text:'小米秒杀'
+									},
+									{
+										src:"../../static/indexNav/7.gif",
+										text:'真心想要'
+									},
+									{
+										src:"../../static/indexNav/8.gif",
+										text:'电视特卖'
+									},
+									{
+										src:"../../static/indexNav/9.gif",
+										text:'家电热卖'
+									},
+									{
+										src:"../../static/indexNav/10.gif",
+										text:'米粉卡'
+									},
+								]
+							},
+							{
+								type:"threeeAdv",
+								data:{
+										big:{
+											src:"../../static/images/demo/demo1.jpg"
+										},
+										smallTop:{
+											src:"../../static/images/demo/demo2.jpg"
+										},
+										smallBottom:{
+											src:"../../static/images/demo/demo3.jpg"
+										}
+									},
+							},
+							{
+								type:"commonList",
+								data:[
+									{
+										cover:"../../static/images/list/1.jpg",
+										title:"米家空调",
+										desc:"1.5匹变流空调",
+										oPrice:"2678",
+										nPrice:"1999"
+									},
+									{
+										cover:"../../static/images/list/2.jpg",
+										title:"黑鲨手机",
+										desc:"游戏性能，散热好",
+										oPrice:"2678",
+										nPrice:"1299"
+									},
+									{
+										cover:"../../static/images/list/3.jpg",
+										title:"红米手机",
+										desc:"1.5匹变流空调",
+										oPrice:"2678",
+										nPrice:"1999"
+									}
+								]
+							},]
+	
+	import swiperImages from "@/components/index/swiper-images.vue"
+	import navIcon from "@/components/index/index-nav.vue"
+	import threeAdv from "@/components/index/three-adv.vue"
+	import card from "@/components/common/card.vue"
+	import commonList from "@/components/common/commList.vue"
+	export default {
+		components:{
+			swiperImages,
+			navIcon,
+			threeAdv,
+			card,
+			commonList
+		},
+		data() {
+			return {
+				scrollInto:'',
+				tabIndex:0,
+				scrollH:500,
+				tabBars:[],
+				newsItems:[],
+					
 			}
 		},
 		onLoad() {
 		uni.getSystemInfo({
 			success: (res) => {
 				this.scrollH=res.windowHeight-uni.upx2px(80)
-				console.log(res.windowHeight-uni.upx2px(80))
 			}
 		})
+			// 初始化事件
+			this.__init()
 		},
 		methods: {
+			// 初始化事件
+			__init(){
+				// 获取顶部选项卡
+				this.tabBars=demoTabars
+				// 根据顶部选项卡生成页面
+				let arr=[];
+				for (let i = 0; i < this.tabBars.length; i++) {
+					let obj={
+						list:[]
+					}
+					if (i===0) {
+						obj.list=demo1
+					}
+					arr.push(obj)
+				}
+				this.newsItems=arr
+				console.log(this.newsItems)
+			},
+			
+			
 			// 切换选项卡
 				changeTab(index){
 					if(this.tabIndex===index){
@@ -385,6 +347,7 @@
 					}else{
 						this.tabIndex=index
 						this.scrollInto='tab'+index
+						this.addData()
 					}
 					
 				},
@@ -392,9 +355,11 @@
 				onChageTab(e){
 					console.log(e.detail.current)
 					this.changeTab(e.detail.current)
-					console.log(this.newsItems.list)
 				},
-				
+				addData(){
+					let index=this.tabIndex
+					this.newsItems[index].list=demo2
+				}
 				
 				
 		}
