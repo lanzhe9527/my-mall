f500a036c630357cfc62ac5c42a851307fd1d074
Rocky <lanzhe9527@163.com>
2023-09-05 23:13:18 +0800

    多色按钮组件封装

---
+++ components/search/color-tag.vue
@@ -0,0 +1,37 @@
+<template>
+		<view class="button" :style="getBgStyle">{{item.name}}</view>
+</template>
+
+<script>
+	export default{
+		props:{
+			item:Object,
+			
+		},
+		computed: {
+			getBgStyle() {
+				let color = {
+					borderColor: ['#ff88c2', '#00aa88', '#0066ff', '#b94fff', '#aaaaaa', '#ffcc22', '#ff8800'],
+					backgroundColor: ['#ffb7dd', '#00ddaa', '#5599ff', '#d28fff', '#dddddd', '#ffdd55', '#ffaa33'],
+				}
+				let index = Math.floor(Math.random() * color.borderColor.length)
+				let borderColor = color.borderColor[index]
+				let backgroundColor = color.backgroundColor[index]
+				return `borderColor:${borderColor};backgroundColor:${backgroundColor}`
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.button-box {
+		padding: 10px;
+	}
+	
+	.button {
+		display: inline-block;
+		padding: 2px 10px;
+		border: 2px solid lightgray;
+		margin: 5px 10px;
+	}
+</style>
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,22 +1,39 @@
 <template>
 	<view>
-		搜索页
+		<card headerTitle="热门搜索" body-img="../../static/images/demo/cate_banner.png"></card>
+		<!-- 多色按钮 -->
+		<view class="button-box">
+				<color-tag :item="item" v-for="(item,index) in hot" :key="index"></color-tag>
+		</view>
 	</view>
 </template>
 
 <script>
+	import card from "@/components/common/card.vue"
+	import colorTag from "@/components/search/color-tag.vue"
 	export default {
+		components: {
+			card,
+			colorTag,
+		},
 		data() {
 			return {
-				
+				hot:[
+					{name:'领券中心'},
+					{name:'LC250越野车'},
+					{name:'Jeep牧马人'},
+					{name:'tank500Hi4-t'},
+					{name:'福特猛禽烈马'},
+				]
 			}
 		},
+	
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style>
-
+	
 </style>
