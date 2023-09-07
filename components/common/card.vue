d8b4a82192bada26ad3e359f4331fa0933877616
Rocky <lanzhe9527@163.com>
2023-09-07 22:38:22 +0800

    优化卡片组件

--- components/common/card.vue
+++ components/common/card.vue
@@ -1,25 +1,41 @@
 <template>
 	<view class="card-box">
 		<!-- 标题 -->
-		<view class="title-box">
-			<text class="title-text">{{headerTitle}}</text>
+		<view class="title-box" :class="getHeaderClass">
+			<text class="title-text" :class="fontWeight?'font-weight':''">{{headerTitle}}</text>
 		</view>
-		<image :src="bodyImg" mode="widthFix" v-if="bodyImg"
-		style="width: 750rpx;height: 300rpx;">
+		<image :src="bodyImg" mode="widthFix" v-if="bodyImg" style="width: 750rpx;height: 300rpx;">
 		</image>
 		<slot />
 	</view>
 </template>
 
 <script>
-	export default{
-		props:{
-			headerTitle:String,
-			bodyImg:String,
+	export default {
+		props: {
+			headerTitle: String,
+			bodyImg: String,
 			// 是否显示头部
-			showhead:{
-				type:Boolean,
-				default:true
+			showhead: {
+				type: Boolean,
+				default: true
+			},
+			// 文字是否加粗
+			fontWeight: {
+				type: Boolean,
+				default: true
+			},
+			// 是否显示下边线
+			borderBottom: {
+				type: Boolean,
+				default: true
+			},
+
+		},
+		computed: {
+			getHeaderClass() {
+				let rz = this.borderBottom ? 'border-bottom' : ''
+				return `${rz}`
 			}
 		}
 	}
@@ -27,13 +43,23 @@
 
 <style scoped>
 	.title-box {
-		border-bottom: 1rpx solid #f1f1f1;
 		padding: 10rpx 20rpx;
 
 	}
 
 	.title-text {
 		font-size: 30rpx;
+	}
+
+	.border-bottom {
+		border-bottom: 1rpx solid lightgray;
+	}
+
+	.font-weight {
 		font-weight: 500;
+	}
+
+	.color {
+		color: red;
 	}
 </style>
