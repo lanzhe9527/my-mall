a85757bd6ab9e341bc87e61a3a56e7e04eb1546f
Rocky <lanzhe9527@163.com>
2023-09-10 22:34:39 +0800

    左边点击，右边到对应区域

--- pages/class/class.vue
+++ pages/class/class.vue
@@ -1,21 +1,20 @@
 <template>
 	<view style="height: 100%; border-top: 1rpx solid lightgray;box-sizing: border-box;" class="d-flex">
 		<scroll-view scroll-y="true" style="flex: 1;border-right: 1rpx solid lightgray;">
-			<view class="py-1" style="border-bottom: 1rpx solid lightgray;" hover-class="gray-bg-color"
+			<view class="py-1 left-scroll-item" style="border-bottom: 1rpx solid lightgray;" hover-class="gray-bg-color"
 				v-for="(item,index) in cate" :key="index" @tap="selected(index)">
 				<view class="py-1 text-center" :class="activeIndex===index?'slect-active':''">{{item.name}}</view>
 			</view>
 		</scroll-view>
 
-		<scroll-view scroll-y="true" style="flex: 3.5;">
-			<view class="d-flex" style="flex-wrap: wrap;" v-for="(item,index) in list" :key="index">
-				<view class="span24-8 py-2 text-center" v-for="(item2,index2) in item.list" :key="index2">
+		<scroll-view scroll-y="true" style="flex: 3.5;" :scroll-top="rightScrollTop">
+			<view class="d-flex right-scroll-item" style="flex-wrap: wrap;" v-for="(item,index) in list" :key="index">
+				<view class="span24-8 py-2 text-center" v-for="(item2,index2) in item.list"
+					:key="index2">
 					<image :src="item2.src" mode="" style="width: 120rpx;height: 120rpx;">
 					</image>
 					<view class="">{{item2.name}}</view>
 				</view>
-
-
 			</view>
 		</scroll-view>
 
@@ -30,14 +29,38 @@
 				activeIndex: 0,
 				cate: [],
 				list: [],
+				leftDomsTop: [],
+				rightDomsTop: [],
+				rightScrollTop:0,
 			}
 		},
 		onLoad() {
 			this.getData()
 		},
+		onReady() {
+			const query = uni.createSelectorQuery().in(this);
+			const query1 = uni.createSelectorQuery().in(this);
+			query.selectAll('.left-scroll-item').boundingClientRect(data => {
+				console.log(data)
+				this.leftDomsTop = data.map(v => {
+					return v.top
+				})
+				console.log(this.leftDomsTop)
+			}).exec();
+
+			query1.selectAll('.right-scroll-item').boundingClientRect(data => {
+				console.log(data)
+				this.rightDomsTop = data.map(v => {
+					return v.top
+				})
+				console.log(this.rightDomsTop)
+			}).exec();
+
+		},
 		methods: {
 			selected(index) {
 				this.activeIndex = index
+				this.rightScrollTop=this.rightDomsTop[index]
 			},
 			getData() {
 				for (let i = 1; i < 20; i++) {
@@ -53,11 +76,11 @@
 					for (let j = 0; j < 10; j++) {
 						this.list[i].list.push({
 							src: "../../static/images/demo/cate_07.png",
-							name:`分类${i}-商品${j}`
+							name: `分类${i+1}-商品${j}`
 						})
 					}
 				}
-				
+
 			}
 		}
 	}
