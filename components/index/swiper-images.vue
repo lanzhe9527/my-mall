4fbd4e7148a29df73a6dad34aa25de26a862163e
Rocky <lanzhe9527@163.com>
2023-09-12 08:50:34 +0800

    格式化代码

--- components/index/swiper-images.vue
+++ components/index/swiper-images.vue
@@ -2,33 +2,33 @@
 	<view>
 		<!-- 轮播图 -->
 		<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="gray"
-				indicator-active-color="#FFF" circular :style="getStyle">
-				<block v-for="(item,index) in resdata" :key="index">
-					<swiper-item>
-						<view class="swiper-item">
-							<image :src="item.src" :style="getStyle"></image>
-						</view>
-					</swiper-item>
-				</block>
+			indicator-active-color="#FFF" circular :style="getStyle">
+			<block v-for="(item,index) in resdata" :key="index">
+				<swiper-item>
+					<view class="swiper-item">
+						<image :src="item.src" :style="getStyle"></image>
+					</view>
+				</swiper-item>
+			</block>
 		</swiper>
 	</view>
 </template>
 
 <script>
-	export default{
-		props:{
-			resdata:Array,
-			height:{
-				type:String,
-				default:"350rpx"
+	export default {
+		props: {
+			resdata: Array,
+			height: {
+				type: String,
+				default: "350rpx"
 			},
 		},
-		computed:{
-			getStyle(){
-			return `height: ${this.height}rpx;`	
+		computed: {
+			getStyle() {
+				return `height: ${this.height}rpx;`
 			}
 		},
-		
+
 	}
 </script>
 
