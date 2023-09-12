48b6788c7b7078716db437e4b6823af8e21e4d2d
Rocky <lanzhe9527@163.com>
2023-09-12 08:49:16 +0800

    优化轮播组件

--- components/index/swiper-images.vue
+++ components/index/swiper-images.vue
@@ -2,11 +2,11 @@
 	<view>
 		<!-- 轮播图 -->
 		<swiper indicator-dots autoplay :interval="3000" :duration="1000" indicator-color="gray"
-				indicator-active-color="#FFF" circular>
+				indicator-active-color="#FFF" circular :style="getStyle">
 				<block v-for="(item,index) in resdata" :key="index">
 					<swiper-item>
 						<view class="swiper-item">
-							<image :src="item.src" style="height: 350rpx;"></image>
+							<image :src="item.src" :style="getStyle"></image>
 						</view>
 					</swiper-item>
 				</block>
@@ -17,8 +17,18 @@
 <script>
 	export default{
 		props:{
-			resdata:Array
-		}
+			resdata:Array,
+			height:{
+				type:String,
+				default:"350rpx"
+			},
+		},
+		computed:{
+			getStyle(){
+			return `height: ${this.height}rpx;`	
+			}
+		},
+		
 	}
 </script>
 
--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,18 +1,29 @@
 <template>
 	<view>
-		商品详情
+		<!-- 商品详情大图 -->
+		<swiper-image :resdata="banner" height="750"></swiper-image>
 	</view>
 </template>
 
 <script>
+	import swiperImage from "@/components/index/swiper-images.vue"
 	export default {
+		components: {
+			swiperImage,
+		},
 		data() {
 			return {
-				
+				banner: [{
+					src: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1679982565.14646441.png"
+				}, {
+					src: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1681805069.68642151.png"
+				}, {
+					src: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1692003918.717685.jpg"
+				}]
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
