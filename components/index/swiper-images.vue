63c2b0dd94ca651d8a2883b4e0fab8a4e8ad376c
Rocky <lanzhe9527@163.com>
2023-09-12 09:19:12 +0800

    点击详情图片显示大图

--- components/index/swiper-images.vue
+++ components/index/swiper-images.vue
@@ -5,7 +5,7 @@
 			indicator-active-color="#FFF" circular :style="getStyle">
 			<block v-for="(item,index) in resdata" :key="index">
 				<swiper-item>
-					<view class="swiper-item">
+					<view class="swiper-item" @tap="event(item,index)">
 						<image :src="item.src" :style="getStyle"></image>
 					</view>
 				</swiper-item>
@@ -22,13 +22,32 @@
 				type: String,
 				default: "350rpx"
 			},
+			preview:{
+				type:Boolean,
+				default:true
+			}
 		},
 		computed: {
 			getStyle() {
 				return `height: ${this.height}rpx;`
+			},
+			getIUrl(){
+				let arr=this.resdata.map(v=>{
+					return v.src
+				})
+				return arr
 			}
 		},
-
+		methods: {
+			event(item,index) {
+				if (this.preview) {
+					uni.previewImage({
+						current:index,
+						urls:this.getIUrl
+					})
+				}
+			}
+		},
 	}
 </script>
 
