08b1098e6ba56a78df0e76cb05cf0c781b8938f1
Rocky <lanzhe9527@163.com>
2023-09-13 11:22:57 +0800

    详情>滚动商品ui

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -8,12 +8,23 @@
 			<view class="font-md text-gray line-h-sm mb-3">次动力次动力次动力次动力次动力次动力次动力次动力次动力</view>
 			<price price-size="font-lg" unit-size="font">3199</price>
 		</view>
-		
-		
-		
-		
-		
-		
+		<!-- 滚动商品特性 170x110-->
+		<scroll-view scroll-x="true" class="scroll-row">
+			<view class="scroll-row-item" style="width: 170rpx;height:110rpx;" 
+				v-for="(item,index) in baseAttrs" :key="index">
+				<view class="d-flex flex-column a-center j-center" style="height:110rpx;">
+					<view class="line-h-sm iconfont" :class="item.icon"></view>
+					<view class="line-h">{{item.title}}</view>
+					<view class="font-sm text-gray">{{item.desc}}</view>
+				</view>
+			</view>
+
+		</scroll-view>
+
+
+
+
+
 	</view>
 </template>
 
@@ -33,7 +44,20 @@
 					src: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1681805069.68642151.png"
 				}, {
 					src: "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1692003918.717685.jpg"
-				}]
+				}],
+				
+				baseAttrs:[
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					{icon:"icon-saoyisao",title:"cpu",desc:"蛟龙888处理器"},
+					
+				],
+				
 			}
 		},
 		methods: {
