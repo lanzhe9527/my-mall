08257d48fba4eb622a26be7d21bf768846db8671
Rocky <lanzhe9527@163.com>
2023-09-13 09:57:22 +0800

    详情页>基础详情

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -2,14 +2,28 @@
 	<view>
 		<!-- 商品详情大图 -->
 		<swiper-image :resdata="banner" height="750"></swiper-image>
+		<!-- 基础详情 -->
+		<view class="px-2">
+			<view class="font-lg">小米MIX</view>
+			<view class="font-md text-gray line-h-sm mb-3">次动力次动力次动力次动力次动力次动力次动力次动力次动力</view>
+			<price price-size="font-lg" unit-size="font">3199</price>
+		</view>
+		
+		
+		
+		
+		
+		
 	</view>
 </template>
 
 <script>
 	import swiperImage from "@/components/index/swiper-images.vue"
+	import price from "@/components/common/price.vue"
 	export default {
 		components: {
 			swiperImage,
+			price,
 		},
 		data() {
 			return {
@@ -28,6 +42,6 @@
 	}
 </script>
 
-<style>
+<style scoped>
 
 </style>
