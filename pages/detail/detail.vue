c35dd2c8bc113121a5176f17edfd716bd9ee8db7
Rocky <lanzhe9527@163.com>
2023-09-15 17:15:01 +0800

    滑动评论ui

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -42,16 +42,72 @@
 							
 						</view>
 					</uni-list-item>
-					
-					
-					
-					
-					
 				</ui-list>
 			</view>
-	
 		</view>
-
+			<!-- 横向滚动评论 -->
+			<!-- 外620x380
+				头像70
+				图片高115 -->
+			<view class="px-2 py-2">
+				<scroll-view scroll-x="true" scroll-y="false" class="scroll-row">
+					<view class="scroll-row-item bg-gray ridus mr-2 px-2 py-2" style="width: 620rpx;height: 380rpx;">
+						<view class="d-flex a-center">
+							<view class="d-flex a-center">
+								<image src="../../static/images/demo/demo6.jpg" class="mr-2" style="width: 70rpx;height: 70rpx; border-radius: 50%;"></image>
+								<view class="d-flex flex-column text-gray line-h-md">
+									<text>大师兄</text>
+									<text>2023-09-12</text>
+								</view>
+							</view>
+							<view style="margin-left: auto;">
+								<view class="iconfont icon-shanchu text-gray">164</view>
+							</view>
+						</view>
+						<view class="mt-2">评论支持好评评论支持好评评论</view>
+						<view class="d-flex mt-4">
+							<view class="span24-8 px-3">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+							<view class="span24-8 px-2">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+							<view class="span24-8 px-2">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+						</view>
+					</view>
+					
+					<view class="scroll-row-item bg-gray ridus mr-2 px-2 py-2" style="width: 620rpx;height: 380rpx;">
+						<view class="d-flex a-center">
+							<view class="d-flex a-center">
+								<image src="../../static/images/demo/demo6.jpg" class="mr-2" style="width: 70rpx;height: 70rpx; border-radius: 50%;"></image>
+								<view class="d-flex flex-column text-gray line-h-md">
+									<text>大师兄</text>
+									<text>2023-09-12</text>
+								</view>
+							</view>
+							<view style="margin-left: auto;">
+								<view class="iconfont icon-shanchu text-gray">164</view>
+							</view>
+						</view>
+						<view class="mt-2">评论支持好评评论支持好评评论</view>
+						<view class="d-flex mt-4">
+							<view class="span24-8 px-3">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+							<view class="span24-8 px-2">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+							<view class="span24-8 px-2">
+								<image src="../../static/images/demo/demo6.jpg" style="height: 115rpx;"></image>
+							</view>
+						</view>
+					</view>
+					
+				
+				</scroll-view>
+			</view>
 
 
 
