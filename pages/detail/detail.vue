1413118e48c9c2e3c7aee3302b52a7433fffa9f0
Rocky <lanzhe9527@163.com>
2023-09-19 10:28:40 +0800

    开发弹出层组件

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -10,7 +10,7 @@
 		<view class="px-2 py-2">
 			<view class="gray-bg-color">
 				<ui-list>
-					<uni-list-item showArrow :customStyle="{ backgroundColor: '#F1F1F1',padding:''}">
+					<uni-list-item showArrow :customStyle="{ backgroundColor: '#F1F1F1',padding:''}"clickable @click="show">
 						<view class="font-md">
 							<text class="mr-2">已选</text>
 							<text>火焰红 64GB 标配</text>
@@ -57,6 +57,12 @@
 		<!-- 底部操作条 -->
 		<bottom-btn></bottom-btn>
 		
+		<!-- 弹出层 -->
+		<view class="_popup" :class="popupClass">
+			<view class="_mask" @tap.stop='hide'></view>
+			<view class="_body">内容</view>
+		</view>
+		
 	</view>
 </template>
 
@@ -81,6 +87,8 @@
 		},
 		data() {
 			return {
+				popupClass:'none',
+				
 				// 评论
 				comments:[{
 					userpic:"../../static/images/demo/demo6.jpg",	
@@ -178,11 +186,86 @@
 			}
 		},
 		methods: {
-
+			show(){
+				console.log(1)
+				this.popupClass='show'
+			},
+			hide(){
+				this.popupClass='hide'
+				setTimeout(()=>{
+					this.popupClass='none'
+				}, 200);
+			},
+			
 		}
 	}
 </script>
 
 <style scoped>
+._popup,._mask{
+	position: fixed;
+	top: 0;
+	width: 100%;
+	height: 100%;
+}
+
+._popup{
+	z-index: 2000;
+}
+
+._mask{
+	z-index: 2001;
+	background-color: rgba(0, 0, 0, 0.5);
+}
+._body{
+	z-index: 2002;
+	background-color: #fff;
+	position: fixed;
+	bottom: -1035rpx;
+	width: 92%;
+	height: 1035rpx;
+	padding: 0 4%;
+	border-radius: 20rpx 20rpx 0 0;
+}
+
+@keyframes showBody{
+	0{transform: translateY(0);}
+	100%{transform: translateY(-100%)}
+}
+@keyframes hideBody{
+	0{transform: translateY(-100%);}
+	100%{transform: translateY(0)}
+}
+@keyframes showMask{
+	0{opacity: 0;}
+	100%{opacity: 1;}
+}
+@keyframes hideMask{
+	0{opacity: 1;}
+	100%{opacity: 0;}
+}
+
+
+.show{
+	display: block;
+}
+.show ._mask{
+	animation: showMask 0.2s linear both;
+}
+.show ._body{
+	animation: showBody 0.2s linear both;
+}
+.hide{
+	display: block;
+}
+.hide ._body{
+	animation: hideBody 0.2s linear both;
+}
+.hide ._mask{
+	animation: hideMask 0.2s linear both;
+}
+.none{
+	display: none;
+}
 
 </style>
